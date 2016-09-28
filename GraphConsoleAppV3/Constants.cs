22ab628370ec28d46bc5b76afff332e748ed9c32
Calvin Li <calvli@Microsoft.com>
2016-09-27 17:04:23 -0700

    changed config file to have demo constants

--- GraphConsoleAppV3/Constants.cs
+++ GraphConsoleAppV3/Constants.cs
@@ -2,24 +2,17 @@
 {
     internal class Constants
     {
-        public const string TenantNameAppOnly = "calvliTest.onMicrosoft.com";
-        public const string TenantIdAppOnly = "2ea86d85-3149-476d-bd4a-1cc746ef2f01";
-        public const string ClientId = "a7ec6203-e14c-43c9-9369-c044476a3be0";
-        public const string ClientSecret = "eeveaj0RSnQRheEdzVDqQcOAAibL1oJxJs7zhgMCypQ=";
-        //public const string ClientIdForUserAuthn = "66133929-66a4-4edc-aaee-13b04b03207d";
+        public const string TenantNameAppOnly = "GraphDir1.onMicrosoft.com";
+        public const string TenantIdAppOnly = "4fd2b2f2-ea27-4fe5-a8f3-7b1a7c975f34";
+        public const string ClientId = "118473c2-7619-46e3-a8e4-6da8d5f56e12";
+        public const string ClientSecret = "hOrJ0r0TZ4GQ3obp+vk3FZ7JBVP+TX353kNo6QwNq7Q=";
         public const string AuthStringAppOnly = "https://login.microsoftonline.com/" + TenantNameAppOnly;
-        //public const string ResourceUrl = "https://graph.windows.net";
 
-        //PROD
-        //public const string TenantName = "adatumisv.onmicrosoft.com";
-        public const string TenantId = "2ea86d85-3149-476d-bd4a-1cc746ef2f01";
-        public const string TenantName = "Calvin Li";
-        //public const string TenantId = "a830edad9050849NDA1.onmicrosoft.com";
+        public const string TenantId = "7b0208e6-0f64-47ae-bb8b-3e1ba480e347";
+        public const string TenantName = "myorganization";
 
-        //public const string ClientId = "27dd62a3-ba34-4833-b5c4-5698457436c1";
-        //public const string ClientSecret = "moERmT+kyQDJio17R4Gb+TINZ2byJtqZ27slxdcryCs=";
-        //public const string ClientIdForUserAuthn = "a211bee5-17a5-41d0-83be-d02c51536a3e";
-        public const string ClientIdForUserAuthn = "e99fe685-a49f-44af-91cc-7c67d9f73fe1";
+
+        public const string ClientIdForUserAuthn = "2e90cc6b-df6e-4dd0-92d4-77cddc3b9488";
         public const string ClientSecretForUserAuthn = "moERmT+kyQDJio17R4Gb+TINZ2byJtqZ27slxdcryCs=";
         public const string AuthString = "https://login.microsoftonline.com/common";
         public const string ResourceUrl = "https://graph.windows.net";
