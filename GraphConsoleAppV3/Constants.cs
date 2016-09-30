82d421eeb1738ddd81a2852377d0463edac58363
calvli <calvli@microsoft.com>
2016-09-30 13:41:34 -0700

    New format for Constants.cs, also adding in GraphServiceObjectId

--- GraphConsoleAppV3/Constants.cs
+++ GraphConsoleAppV3/Constants.cs
@@ -1,20 +1,25 @@
-﻿namespace GraphConsoleAppV3
+namespace GraphConsoleAppV3
 {
-    internal class Constants
+    internal class AppModeConstants
     {
-        public const string TenantNameAppOnly = "GraphDir1.onMicrosoft.com";
-        public const string TenantIdAppOnly = "4fd2b2f2-ea27-4fe5-a8f3-7b1a7c975f34";
         public const string ClientId = "118473c2-7619-46e3-a8e4-6da8d5f56e12";
         public const string ClientSecret = "hOrJ0r0TZ4GQ3obp+vk3FZ7JBVP+TX353kNo6QwNq7Q=";
-        public const string AuthStringAppOnly = "https://login.microsoftonline.com/" + TenantNameAppOnly;
+        public const string TenantName = "GraphDir1.onMicrosoft.com";
+        public const string TenantId = "4fd2b2f2-ea27-4fe5-a8f3-7b1a7c975f34";
+        public const string AuthString = GlobalConstants.AuthString + TenantName;
+    }
 
-        public const string TenantId = "7b0208e6-0f64-47ae-bb8b-3e1ba480e347";
-        public const string TenantName = "myorganization";
+    internal class UserModeConstants
+    {
+        public const string TenantId = AppModeConstants.TenantId;
+        public const string ClientId = "66133929-66a4-4edc-aaee-13b04b03207d";
+        public const string AuthString = GlobalConstants.AuthString + "common/";
+    }
 
-
-        public const string ClientIdForUserAuthn = "2e90cc6b-df6e-4dd0-92d4-77cddc3b9488";
-        public const string ClientSecretForUserAuthn = "moERmT+kyQDJio17R4Gb+TINZ2byJtqZ27slxdcryCs=";
-        public const string AuthString = "https://login.microsoftonline.com/common";
+    internal class GlobalConstants
+    {
+        public const string AuthString = "https://login.microsoftonline.com/";        
         public const string ResourceUrl = "https://graph.windows.net";
+        public const string GraphServiceObjectId = "00000002-0000-0000-c000-000000000000";
     }
 }
