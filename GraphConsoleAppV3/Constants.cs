858049b59aefbd55690ac5620488798e078301cc
calvli <calvli@microsoft.com>
2016-10-19 10:38:38 -0700

    Updated ClientSecret

--- GraphConsoleAppV3/Constants.cs
+++ GraphConsoleAppV3/Constants.cs
@@ -3,7 +3,7 @@ namespace GraphConsoleAppV3
     internal class AppModeConstants
     {
         public const string ClientId = "118473c2-7619-46e3-a8e4-6da8d5f56e12";
-        public const string ClientSecret = "hOrJ0r0TZ4GQ3obp+vk3FZ7JBVP+TX353kNo6QwNq7Q=";
+        public const string ClientSecret = "tdzicjqRJ2R1Hnvn3AL1aDU+yQjX5oIHz0th0qBeOOI=";
         public const string TenantName = "GraphDir1.onMicrosoft.com";
         public const string TenantId = "4fd2b2f2-ea27-4fe5-a8f3-7b1a7c975f34";
         public const string AuthString = GlobalConstants.AuthString + TenantName;
